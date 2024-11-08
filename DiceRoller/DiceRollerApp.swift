c5cb3f584f6dc8639ba89cc4c85478c83dcb1702
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-08 11:23:29 +0900

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by 東郷翔士希 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
